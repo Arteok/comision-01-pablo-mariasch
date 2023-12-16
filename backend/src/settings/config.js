52442f2544ebe749abec3e60c281932a531a0f5c
Pablo Mariasch <103390999+Arteok@users.noreply.github.com>
2023-12-16 00:04:48 -0300

    desbugueando

--- backend/src/settings/config.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-
-export const config = {
-  port: process.env.PORT || 4000,
-  mongo: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/express-mongo',
-  jwt_secret: process.env.JWT_SECRET || 'secret',
-  database: process.env.DATABASE_NAME || 'playlist-app',
-};
--- frontend/src/utils/consts.js
+++
@@ -1,2 +0,0 @@
-//export const API_URL = "https://playlist-app-backend-production.up.railway.app/api";
-export const API_URL = "http://localhost:4000/api";
